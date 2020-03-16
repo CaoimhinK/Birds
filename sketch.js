28a5a3ced6da2f8a5bb2a0d8e9732012bf3e08ac
Kevin Kahlke <kk@neopoly.de>
2020-03-16 16:30:38 +0100

    added new layers a.s.o.

--- sketch.js
+++ sketch.js
@@ -1,32 +1,58 @@
 
+var xam = 400;
+
 function sigmoid(x) {
   return (1/(1+math.exp(-x))*2-1);
 }
 
+function linear(x) {
+  return constrain(x,-1,1);
+}
+
 class Brain {
-  constructor(mat1, mat2) {
-    if (!(mat1 && mat2)) {
+  constructor(mat1, mat2, mat3, mat4) {
+    if (!(mat1 && mat2 && mat3 && mat4)) {
       let mat1 = [];
-      for (let i = 0; i < 2; i++) {
+      for (let i = 0; i < 5; i++) {
         let row = [];
-        for (let j = 0; j < 5; j++) {
+        for (let j = 0; j < 10; j++) {
           row.push(random()*2-1);
         }
         mat1.push(row);
       }
       this.mat1 = math.matrix(mat1);
       let mat2 = [];
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < 10; i++) {
         let row = [];
-        for (let j = 0; j < 2; j++) {
+        for (let j = 0; j < 10; j++) {
           row.push(random()*2-1);
         }
         mat2.push(row);
       }
       this.mat2 = math.matrix(mat2);
+      let mat3 = [];
+      for (let i = 0; i < 10; i++) {
+        let row = [];
+        for (let j = 0; j < 10; j++) {
+          row.push(random()*2-1);
+        }
+        mat3.push(row);
+      }
+      this.mat3 = math.matrix(mat3);
+      let mat4 = [];
+      for (let i = 0; i < 10; i++) {
+        let row = [];
+        for (let j = 0; j < 3; j++) {
+          row.push(random()*2-1);
+        }
+        mat4.push(row);
+      }
+      this.mat4 = math.matrix(mat4);
     } else {
       this.mat1 = mat1;
       this.mat2 = mat2;
+      this.mat3 = mat3;
+      this.mat4 = mat4;
     }
   }
 
@@ -35,15 +61,19 @@ class Brain {
   }
 
   process() {
-    let hidden = math.multiply(this.input, this.mat1);
-    hidden = hidden.map((value, index, matrix) => {
-      return sigmoid(value);
+    let hidden1 = math.multiply(this.input, this.mat1);
+    hidden1 = hidden1.map((value, index, matrix) => {
+      return linear(value);
     });
-    let out = math.multiply(hidden, this.mat2);
-    let outVec = p5.Vector.fromAngle(out.get([0]));
-    let outMag = out.get([1]);
-    outVec.setMag(outMag);
-    this.output = outVec;
+    let hidden2 = math.multiply(hidden1, this.mat2);
+    hidden2 = hidden2.map((value, index, matrix) => {
+      return linear(value);
+    });
+    let hidden3 = math.multiply(hidden2, this.mat3);
+    hidden3 = hidden3.map((value, index, matrix) => {
+      return linear(value);
+    });
+    this.output = math.multiply(hidden3, this.mat4);
   }
 
   getOutput() {
@@ -51,21 +81,35 @@ class Brain {
   }
 
   copy() {
-    return new Brain(this.mat1, this.mat2);
+    return new Brain(this.mat1, this.mat2, this.mat3, this.mat4);
   }
 
   mutate() {
     this.mat1 = math.map(this.mat1, (value) => {
       const chance = random();
       if (chance < 0.05) {
-        return value + random(0.5);
+        return value + random(-0.5, 0.5);
       }
       return value;
     });
     this.mat2 = math.map(this.mat2, (value) => {
       const chance = random();
       if (chance < 0.05) {
-        return value + random(0.5);
+        return value + random(-0.5, 0.5);
+      }
+      return value;
+    });
+    this.mat3 = math.map(this.mat3, (value) => {
+      const chance = random();
+      if (chance < 0.05) {
+        return value + random(-0.5, 0.5);
+      }
+      return value;
+    });
+    this.mat4 = math.map(this.mat4, (value) => {
+      const chance = random();
+      if (chance < 0.05) {
+        return value + random(-0.5, 0.5);
       }
       return value;
     });
@@ -74,28 +118,39 @@ class Brain {
 
 
 class Bird {
-  constructor(pos, brain) {
-    this.pos = pos || createVector(0,0);
+  constructor(pos, brain, col) {
+    this.pos = pos || createVector(width/2,height/2);
     this.vel = createVector(0,0);
+    this.acc = createVector(0,0);
     this.brain = brain || new Brain();
+    this.color = col || color(0,100,100);
   }
 
-  update(targetDirection) {
-    let dirVec = targetDirection.normalize();
-    this.brain.setInput([dirVec.x, dirVec.y]);
+  update(targetPosition) {
+    let targetDirection = p5.Vector.sub(targetPosition, this.pos);
+    let distanceInput = targetDirection.mag()/(width*width);
+    let dirInput = p5.Vector.sub(targetDirection.normalize(), this.vel.normalize());
+    let velNorm = this.vel.normalize();
+    this.brain.setInput([velNorm.x, velNorm.y, dirInput.x, dirInput.y, distanceInput]);
     this.brain.process();
-    this.vel = this.brain.getOutput();
+    let [directionX, directionY, magnitude] = this.brain.getOutput().toArray();
+    this.acc = createVector(directionX, directionY);
+    this.acc.setMag(magnitude);
+    this.vel.add(this.acc);
+    this.vel.limit(3);
     this.pos.add(this.vel);
   }
 
   show() {
-    ellipse(this.pos.x, this.pos.y, 10,10);
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, 5,5);
   }
 
   reproduce() {
     const newBrain = this.brain.copy();
     newBrain.mutate();
-    return new Bird(createVector(0,0), newBrain);
+    let newHue = hue(this.color) + 10;
+    return new Bird(null, newBrain, color(newHue, 100, 100));
   }
 
   computeFitness(target) {
@@ -110,24 +165,25 @@ var cycleTime;
 function setup() {
   createCanvas(500,500);
   noStroke();
-  fill(255);
+  colorMode(HSB, 360, 100, 100);
   frameRate(60);
   birds = [];
   cycleTime = 0;
 
-  for (let i = 0; i < 50; i++) {
-    birds[i] = new Bird(createVector(0, 0));
+  for (let i = 0; i < xam; i++) {
+    birds[i] = new Bird(createVector(width/2, height/2));
   }
-  target = createVector(mouseX, mouseY);
+  target = createVector(random(width), random(height));
 }
 
 function draw() {
   background(0);
   for (let i = 0; i < birds.length; i++) {
-    birds[i].update(p5.Vector.sub(target,birds[i].pos));
+    birds[i].update(target);
     birds[i].show();
   }
-  target = createVector(mouseX, mouseY);
+  fill(0,0,100);
+  ellipse(target.x, target.y, 5,5);
   if (cycleTime >= 5000) {
     birds.forEach((bird) => {
       bird.computeFitness(target);
@@ -135,14 +191,21 @@ function draw() {
     birds.sort((bird, other) => {
       return other.fitness - bird.fitness;
     });
+    console.log(birds[0].fitness);
     const newBirds = [];
-    birds.slice(0, birds.length/2).forEach((bird) => {
-      bird.pos = createVector(0,0);
-      newBirds.push(bird);
-      newBirds.push(bird.reproduce());
-    });
-    birds = newBirds;
+    const oldBirds = [];
+    for (let i = 0; i < xam/4; i++) {
+      oldBirds.push(new Bird(null, birds[i].brain.copy()))
+      for (let j = 0; j < (xam/4)-i; j++) {
+        newBirds.push(birds[i]);
+      }
+    }
+    birds = oldBirds;
+    for (let i = 0; i < 3*xam/4; i++) {
+      birds.push(random(newBirds).reproduce());
+    }
     cycleTime = 0;
+    target = createVector(random(width), random(height));
   }
   cycleTime += deltaTime;
 }
